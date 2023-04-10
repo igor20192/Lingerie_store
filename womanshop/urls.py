26b173801155980e6efeed5d7ba266bbc747730f
igor20192 <igor.udovenko2012@meta.ua>
2023-04-10 22:19:04 +0300

    Implemented user profile editing

--- womanshop/urls.py
+++ womanshop/urls.py
@@ -1,7 +1,18 @@
 from django.urls import path
-
+from django.contrib.auth.decorators import login_required
+from django.views.generic import TemplateView
 from . import views
 
 urlpatterns = [
-    path("", views.index, name="index"),
+    path("", TemplateView.as_view(template_name="womanshop/index.html"), name="index"),
+    path(
+        "user_profile/",
+        login_required(views.UserProfileView.as_view()),
+        name="user_profile",
+    ),
+    path(
+        "user_profile_form/",
+        views.UserProfileFormView.as_view(),
+        name="user_profile_form",
+    ),
 ]
