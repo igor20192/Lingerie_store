55de1a7c323540c575630b66ed10abae50ed74cd
igor20192 <igor.udovenko2012@meta.ua>
2023-04-12 14:59:03 +0300

    add models category,style,color,brand,produkt

--- womanshop/admin.py
+++ womanshop/admin.py
@@ -1,6 +1,12 @@
 from django.contrib import admin
-from .models import UserProfile, Order
+from .models import UserProfile, Order, Category, Style, Brand, Size, Color, Product
 
 # Register your models here.
 admin.site.register(UserProfile)
 admin.site.register(Order)
+admin.site.register(Category)
+admin.site.register(Style)
+admin.site.register(Brand)
+admin.site.register(Size)
+admin.site.register(Color)
+admin.site.register(Product)
--- womanshop/models.py
+++ womanshop/models.py
@@ -41,3 +41,62 @@ class Order(models.Model):
 
     def __str__(self):
         return self.order_number
+
+
+class Category(models.Model):
+    name = models.CharField(max_length=100)
+
+    def __str__(self):
+        return self.name
+
+
+class Style(models.Model):
+    name = models.CharField(max_length=100)
+
+    def __str__(self):
+        return self.name
+
+
+class Brand(models.Model):
+    name = models.CharField(max_length=100)
+
+    def __str__(self):
+        return self.name
+
+
+class Color(models.Model):
+    name = models.CharField(max_length=100)
+
+    def __str__(self):
+        return self.name
+
+
+class Size(models.Model):
+    name = models.CharField(max_length=100)
+
+    def __str__(self):
+        return self.name
+
+
+class Product(models.Model):
+    upload_to = "products/"
+
+    name = models.CharField(max_length=100)
+    category = models.ForeignKey(Category, on_delete=models.CASCADE)
+    style = models.ForeignKey(Style, on_delete=models.CASCADE)
+    brand = models.ForeignKey(Brand, on_delete=models.CASCADE)
+    color = models.ForeignKey(Color, on_delete=models.CASCADE)
+    size = models.ForeignKey(Size, on_delete=models.CASCADE)
+    vendor_code = models.CharField(max_length=6, unique=True)
+    collection = models.CharField(max_length=200)
+    price = models.DecimalField(max_digits=8, decimal_places=2)
+    description = models.TextField()
+    image1 = models.ImageField(upload_to=upload_to)
+    image2 = models.ImageField(upload_to=upload_to)
+    image3 = models.ImageField(upload_to=upload_to)
+    image4 = models.ImageField(upload_to=upload_to)
+    sale = models.BooleanField()
+    created_at = models.DateTimeField(auto_now_add=True)
+
+    def __str__(self):
+        return self.name
