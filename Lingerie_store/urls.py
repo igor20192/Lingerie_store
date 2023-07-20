54b561de09c063b72816b51bfa2a5e296591b806
igor20192 <igor.udovenko2012@meta.ua>
2023-07-20 22:25:35 +0300

    urls.py add path "paypal/"

--- Lingerie_store/urls.py
+++ Lingerie_store/urls.py
@@ -23,6 +23,7 @@ urlpatterns = (
         path("admin/", admin.site.urls),
         path("", include("womanshop.urls")),
         path("accounts/", include("allauth.urls")),
+        path("paypal/", include("paypal.standard.ipn.urls")),
     ]
     + static(settings.STATIC_URL, document_root=settings.STATIC_ROOT)
     + static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
