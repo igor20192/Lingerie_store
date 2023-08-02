814de3563e8074db6895b97b133d00ffd7b4425c
igor20192 <igor.udovenko2012@meta.ua>
2023-08-02 11:39:05 +0300

    base.js add megamenu, search

--- womanshop/static/womanshop/base.js
+++ womanshop/static/womanshop/base.js
@@ -1,9 +1,15 @@
 document.addEventListener("DOMContentLoaded", () => {
     // Получаем элемент с id="brand"
     const brandElement = document.getElementById('brend');
+    const catalogElement = document.getElementById('catalog');
+    const findElement = document.getElementById('group1');
+    const searchInput = document.getElementById('search-input');
+    const searchResults = document.getElementById('search-results');
+
 
     // Получаем элемент с id="brand-dropdown"
     const brandDropdownElement = document.getElementById('brand-dropdown');
+    const megamenuElement = document.getElementById('megamenu')
 
     // Добавляем обработчик события при наведении курсора на элемент с id="brand"
     brandElement.addEventListener('mouseenter', function () {
@@ -16,4 +22,43 @@ document.addEventListener("DOMContentLoaded", () => {
         // При уходе курсора скрываем выпадающий список
         brandDropdownElement.classList.remove('show');
     });
-})
+
+    catalogElement.addEventListener('mouseenter', () => {
+        megamenuElement.classList.add('show');
+    });
+
+    megamenuElement.addEventListener('mouseleave', () => {
+        megamenuElement.classList.remove('show');
+    });
+
+    findElement.addEventListener('click', () => {
+        if (searchInput.style.display === 'none') {
+            searchInput.style.display = 'block';
+        }
+        else {
+            searchInput.style.display = 'none';
+            searchResults.innerHTML = ''
+        }
+    });
+
+
+    searchInput.addEventListener('input', () => {
+        const searchTerm = searchInput.value;
+        if (searchTerm.trim() === '') {
+            searchResults.innerHTML = '';
+            return;
+        }
+
+        // Отправляем AJAX-запрос на сервер с введенным поисковым запросом
+        fetch(`/search/?q=${encodeURIComponent(searchTerm)}`)
+            .then(response => response.json())
+            .then(data => {
+                const resultsHtml = data.map(item => `<li><a href="/product/${item.id}/">${item.name}</a></li>`).join('');
+                searchResults.innerHTML = `<ul>${resultsHtml}</ul>`;
+            })
+            .catch(error => {
+                console.error('Ошибка при выполнении AJAX-запроса:', error);
+            });
+    });
+});
+
