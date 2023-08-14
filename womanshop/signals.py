8208b4d41afefd271ccf09a16070358c28034f3b
igor20192 <igor.udovenko2012@meta.ua>
2023-08-14 19:15:35 +0300

    test signal cutoff

--- womanshop/signals.py
+++ womanshop/signals.py
@@ -1,3 +1,4 @@
+from sys import argv
 from django.db.models.signals import post_save, post_delete
 from django.dispatch import receiver
 from django.contrib.auth.models import User
@@ -14,28 +15,25 @@ from django.core.exceptions import ValidationError
 def create_user_profile(sender, instance, **kwargs):
     """
     Signal post_save that creates a user profile when a new user is created.
-
     Args:
         sender: The model class that initiated the signal.
         instance: The model instance that was saved.
-
     Returns:
         None
     """
-    obj = User.objects.filter(id=instance.id)
-    if not (UserProfile.objects.filter(user_id=instance.id)) and obj:
-        UserProfile.objects.create(user=obj[0])
+    if "test" not in argv:
+        obj = User.objects.filter(id=instance.id)
+        if not (UserProfile.objects.filter(user_id=instance.id)) and obj:
+            UserProfile.objects.create(user=obj[0])
 
 
 @receiver(valid_ipn_received)
 def show_me_the_money(sender, **kwargs):
     """
     Signal valid_ipn_received that handles successful PayPal payments.
-
     Args:
         sender: The class that initiated the signal.
         **kwargs: Additional arguments.
-
     Returns:
         None
     """
@@ -44,7 +42,6 @@ def show_me_the_money(sender, **kwargs):
         if ipn_obj.receiver_email != settings.PAYPAL_BUSINESS:
             # Not a valid payment
             return
-
         order = get_object_or_404(Order, pk=ipn_obj.invoice)
         if (
             ipn_obj.mc_gross == round(Decimal(order.order_total))
@@ -60,11 +57,9 @@ def show_me_the_money(sender, **kwargs):
 def update_stock_on_order_delete(sender, instance, **kwargs):
     """
     Signal post_delete that updates the product stock when an order is deleted.
-
     Args:
         sender: The model class that initiated the signal.
         instance: The model instance that was deleted.
-
     Returns:
         None
     """
